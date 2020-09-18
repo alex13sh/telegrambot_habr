7b44911a8f566da68bb38b5ac2f18fc00ebbd54a
alex13sh <alex97sh@yandex.ru>
2020-09-19 00:52:50 +0300

    Добавил ещё одно состояние - выбор сайта

--- handlers/subs.py
+++ handlers/subs.py
@@ -4,8 +4,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from misc import dp
 
 class OrderSubs(StatesGroup):
+    waiting_for_select_website = State()
     waiting_for_subs_name = State()
-    #waiting_for_food_size = State()
     
 # Команда активации подписки
 @dp.message_handler(commands='subscribe', state='*')
@@ -16,11 +16,22 @@ async def subscribe(message: types.Message):
     if name: 
         await message.answer("Вы успешно подписались на рассылку! По имени: "+name)
     else:
-        await message.answer("На кого хотите подписаться? Укажите имя:")
-        await OrderSubs.waiting_for_subs_name.set()
+        await OrderSubs.waiting_for_select_website.set()
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        keyboard.add("Habr", "StopGame")
+        await message.answer("На кого хотите подписаться? Выберите сайт:", reply_markup=keyboard)
 
+@dp.message_handler(state=OrderSubs.waiting_for_select_website, content_types=types.ContentTypes.TEXT)
+async def subscribe_website(message: types.Message, state: FSMContext):  # обратите внимание, есть второй аргумент
+    website = message.text
+    await state.update_data(website=website)
+    await OrderSubs.next()
+    await message.answer("На кого хотите подписаться? Укажите имя:")
+    
 @dp.message_handler(state=OrderSubs.waiting_for_subs_name, content_types=types.ContentTypes.TEXT)
 async def subscribe_name(message: types.Message, state: FSMContext):  # обратите внимание, есть второй аргумент
+    data = await state.get_data()
+    website = data["website"]
     name = message.text
-    await message.answer("Вы успешно подписались на рассылку! По имени: "+name)
+    await message.answer(f"Вы успешно подписались на рассылку! На сайт {website} По имени: {name}")
     await state.finish()
